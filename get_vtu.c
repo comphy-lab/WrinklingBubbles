26d85dece098f69034fb12470a154fae08f1c985
SaumiliJana <jsaumili@gmail.com>
2024-10-26 00:50:43 +0530

    update for paraview viz

---
+++ get_vtu.c
@@ -0,0 +1,26 @@
+#include "grid/octree.h"//3d grid
+#include "navier-stokes/centered.h"
+#include "fractions.h"
+#include "output_vtu_foreach.h"
+
+scalar f[];
+char filename[80];
+
+int main(int a, char const *arguments[])
+{
+  sprintf (filename, "%s", arguments[1]);
+
+  restore (file = filename);
+  //Boundary conditions
+  //velocity
+  u.t[left] = dirichlet(0.0);
+  u.n[left] = dirichlet(0.0);
+  u.r[left] = dirichlet(0.0);
+  f.prolongation = fraction_refine;
+  boundary(all);
+
+  output_vtu((scalar *) {f, p}, (vector *) {u}, filename);
+
+
+}
+
