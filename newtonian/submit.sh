bf905cd4c9e549a7e2347b78a160ffd9b966cf25
SaumiliJana <jsaumili@gmail.com>
2024-12-20 13:23:14 +0530

    updated working slurm script for 3d cases job submission paramshakti

--- newtonian/submit.sh
+++ newtonian/submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## Submission script for Cluster
 #SBATCH --job-name=wrinklingbub3d
-#SBATCH --time=10:00:00 # hh:mm:ss
+#SBATCH --time=00:05:00 # hh:mm:ss
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=40
 #SBATCH --cpus-per-task=1
@@ -19,11 +19,11 @@ source ~/.bashrc
 #export PATH="~/.local/bin:$PATH"
 #
 
-Bo="0.0"
-Oh="10"
-k="10"
+Bo="0.001"
+Oh="50"
+k="30"
 
 export OMP_NUM_THREADS=80
-qcc -fopenmp -O2 -disable-dimensions wrinkling_bub_3d_v1.c -o wrinkling_bub_3d_v1 -lm
-./wrinkling_bub_3d_v2 $Bo $Oh $k
+#qcc -fopenmp -O2 -disable-dimensions wrinkling_bub_3d_v1.c -o wrinkling_bub_3d_v1 -lm
+./wrinkling_bub_3d_v1 $Bo $Oh $k
 
