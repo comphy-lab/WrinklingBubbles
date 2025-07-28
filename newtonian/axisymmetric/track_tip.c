efc8eaf7261ca80c391a24110994a05207060851
SaumiliJana <jsaumili@gmail.com>
2025-07-28 15:16:24 +0200

    flipped cases tip

--- newtonian/axisymmetric/track_tip.c
+++ newtonian/axisymmetric/track_tip.c
@@ -24,7 +24,7 @@ double vel;
 char filename[80], NameOutput[80];//inut file and file with output respetively
 float Oh, k;
 bool flag = 0; //flag is 1 when (x_tip,y_tip) is assigned - to avoid multiple assignment
-
+bool flip = 0;
 int main(int a, char const *arguments[]){
   sprintf(filename, "%s", arguments[1]);
   sprintf(NameOutput, "%s", arguments[2]);
@@ -108,8 +108,7 @@ int main(int a, char const *arguments[]){
     fprintf(ferr, "film tip reached the bottom");
     return 1;
   }
-  double r_c = sqrt((sq(x1-x2)+sq(y1-y2))/2);
-  
+
   if (Oh>1){
     fprintf(ferr, "Collapsing Bubble");
     x_tip = x2;
@@ -117,7 +116,8 @@ int main(int a, char const *arguments[]){
     flag = 1;
   }
 
-foreach(){
+  //track (x3,y3)
+  foreach(){
     if ((flag != 1)&&(f[] > f_thresh) && (f[] < 1. - f_thresh) && (d[] == MainPhase)){
       coord n1 = facet_normal (point, f, s);
       double alpha1 = plane_alpha (f[], n1);
@@ -135,6 +135,45 @@ foreach(){
     }
   }
 
+  double r_2c = sqrt((sq(x1-x2)+sq(y1-y2))/2);
+  double r_3c = sqrt((sq(x1-x3)+sq(y1-y3))/2);
+  double x_t = x1-2*r_3c; //max threshold for flip
+  double delta = 1e-2;
+  double y_tMax = -HUGE;
+  double y_tMin = HUGE;
+  double y_av, x_c, y_c, theta;
+  
+  if ((flag != 1)&&(x2<x_t)){//flipped film
+    flip = 1;
+    foreach(){
+      if ((flag != 1)&&(f[] > f_thresh) && (f[] < 1. - f_thresh) && (d[] == MainPhase)){
+        coord n1 = facet_normal (point, f, s);
+        double alpha1 = plane_alpha (f[], n1);
+        coord segment1[2];
+        if (facets (n1, alpha1, segment1) == 2){
+          double xp2 = x + (segment1[0].x+segment1[1].x)*Delta/2.;
+          double yp2 = y + (segment1[0].y+segment1[1].y)*Delta/2.;
+          
+          if ((xp2>=x2-delta)&&(xp2<x2+delta)&&(yp2>y_tMax)){
+            y_tMax = yp2;
+          }
+          if ((xp2>=x2-delta)&&(xp2<x2+delta)&&(yp2<y_tMin)){
+            y_tMin = yp2;
+          }
+        }
+      }
+    }
+    y_av = 0.5*(y_tMin+y_tMax);
+    x_c = x3;
+    y_c = y1;
+    theta = atan((x_c-x2)/(y_c-y_av));
+
+    x_tip = x_c + r_3c*sin(theta);
+    y_tip = y_c + r_3c*cos(theta);
+  }
+  /*else if (flag != 1){
+
+  }*/
   //fprintf(ferr, "xTip %3.2e, YTip %g\n", x_tip, yMin);
   //return 1;
 
@@ -143,12 +182,12 @@ foreach(){
   restore (file = filename);
 
   if (t == 0){
-    fprintf(ferr, "t x1 y1 x2 y2 x3 y3 x_tip y_tip r_c\n");
-    fprintf(fp, "t x1 y1 x2 y2 x3 y3 x_tip y_tip r_c\n");    
+    fprintf(ferr, "t x1 y1 x2 y2 x3 y3 x_tip y_tip r_2c r_3c x_t\n");
+    fprintf(fp, "t x1 y1 x2 y2 x3 y3 x_tip y_tip r_2c r_3c x_t\n");    
   }
   
-  fprintf(ferr, "%6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e\n",t, x1, y1, x2, y2, x3, y3, x_tip, y_tip, r_c);
-  fprintf(fp, "%6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e\n", t, x1, y1, x2, y2, x3, y3, x_tip, y_tip, r_c);
+  fprintf(ferr, "%6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e\n",t, x1, y1, x2, y2, x3, y3, x_tip, y_tip, r_2c, r_3c, x_t);
+  fprintf(fp, "%6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e %6.5e\n", t, x1, y1, x2, y2, x3, y3, x_tip, y_tip, r_2c, r_3c, x_t);
   fclose(fp);
 
 }
