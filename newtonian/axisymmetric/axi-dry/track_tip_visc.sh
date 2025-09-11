9f50a64ffb52958fd1c7c06fbc9778631e316cb1
SaumiliJana <jsaumili@gmail.com>
2025-09-11 14:40:07 +0200

    post-processing viscous film tips Oh>1

---
+++ newtonian/axisymmetric/axi-dry/track_tip_visc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Oh="20"
+k="20"
+tsnap="0.01"
+
+qcc -Wall -O2 track_tip.c -o track_tip -lm
+python3 track_tip.py $Oh $k $tsnap
+python3 plot_tracktip.py $Oh $k
+python3 plot_visctip.py $Oh $k
